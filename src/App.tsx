044dc591aa7eb205a59356124b6a24801d5f1e30
FerchoChainz <a21110132@ceti.mx>
2026-06-23 16:09:11 -0600

    First structure of form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import Form from "./components/Form"
 
 
 function App() {
   return (
-    <div className="bg-red-50">Calories Tracker</div>
+    <>
+      <header className="bg-lime-600 py-3">
+        <div className="max-w-4xl mx-auto flex justify-between">
+
+          <h1 className="text-center text-xl font-bold text-white uppercase">Calories Tracker</h1>
+        </div>
+      </header>
+
+      <section className="bg-lime-500 py-20 px-5">
+        <div className="max-w-4xl mx-auto">
+
+          <Form/>
+
+        </div>
+      </section>
+    </>
   )
 }
 
